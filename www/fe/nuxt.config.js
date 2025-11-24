fcb35d05e902d959f33a922992d7da2c3fb0b997
nampt97 <nam.phanthanh97@gmail.com>
2025-11-24 14:56:14 +0700

    change version

--- www/fe/nuxt.config.js
+++ www/fe/nuxt.config.js
@@ -1,4 +1,4 @@
-const FAVICON_VERSION = "v=1";
+const FAVICON_VERSION = "v=2";
 
 const withFaviconVersion = (path) => `${path}?${FAVICON_VERSION}`;
 
@@ -215,19 +215,19 @@ export default {
   build: {
     loaders: {
       less: {
-		  javascriptEnabled: true
-		},
-	  vue: {
-         prettify: false
+        javascriptEnabled: true
+      },
+      vue: {
+        prettify: false
       }
     },
-	  filenames: {
-		app: ({ isDev, isModern }) => isDev ? `[name]${isModern ? '.modern' : ''}.js` : `[contenthash:7]${isModern ? '.modern' : ''}.js`,
-		chunk: ({ isDev, isModern }) => isDev ? `[name]${isModern ? '.modern' : ''}.js` : `[contenthash:7]${isModern ? '.modern' : ''}.js`,
-		css: ({ isDev }) => isDev ? '[name].css' : 'css/[contenthash:7].css',
-		img: ({ isDev }) => isDev ? '[path][name].[ext]' : 'img/[name].[contenthash:7].[ext]',
-		font: ({ isDev }) => isDev ? '[path][name].[ext]' : 'fonts/[name].[contenthash:7].[ext]',
-		video: ({ isDev }) => isDev ? '[path][name].[ext]' : 'videos/[name].[contenthash:7].[ext]'
+    filenames: {
+      app: ({ isDev, isModern }) => isDev ? `[name]${isModern ? '.modern' : ''}.js` : `[contenthash:7]${isModern ? '.modern' : ''}.js`,
+      chunk: ({ isDev, isModern }) => isDev ? `[name]${isModern ? '.modern' : ''}.js` : `[contenthash:7]${isModern ? '.modern' : ''}.js`,
+      css: ({ isDev }) => isDev ? '[name].css' : 'css/[contenthash:7].css',
+      img: ({ isDev }) => isDev ? '[path][name].[ext]' : 'img/[name].[contenthash:7].[ext]',
+      font: ({ isDev }) => isDev ? '[path][name].[ext]' : 'fonts/[name].[contenthash:7].[ext]',
+      video: ({ isDev }) => isDev ? '[path][name].[ext]' : 'videos/[name].[contenthash:7].[ext]'
     },
     extractCSS: true,
   },
